1aa65704e0ef38bc07ca11f6792e66e6cf27d8a7
GGG <gggkiller2@gmail.com>
2024-12-12 20:44:02 -0300

    add library path patching.

---
+++ src/ExitCodes.cs
@@ -0,0 +1,8 @@
+namespace PatchCil;
+
+internal static class ExitCodes
+{
+    public const int Ok = 0;
+    public const int FileIsNotDotnetAssembly = 2;
+    public const int OutputDirectoryDoesNotExist = 3;
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Collections.Immutable;
 using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.CommandLine.IO;
@@ -12,15 +12,6 @@ using PatchCil;
 
 var rootCommand = new RootCommand("Patches a CIL file to support loading native libraries with the correct paths.");
 
-var printImportsOption = new Option<bool>(
-    name: "--print-imports",
-    description: "Whether libraries loaded through DllImport should be printed at the start of execution.",
-    getDefaultValue: static () => false)
-{
-    Arity = ArgumentArity.ZeroOrOne,
-};
-rootCommand.Add(printImportsOption);
-
 var assemblyPathArgument = new Argument<FileInfo>(
     name: "assembly",
     description: "The assembly to be used for the requested operations.")
@@ -30,13 +21,83 @@ var assemblyPathArgument = new Argument<FileInfo>(
     .ExistingOnly();
 rootCommand.Add(assemblyPathArgument);
 
+var printNeededOption = new Option<bool>(
+    name: "--print-needed",
+    description: "Whether libraries loaded through DllImport should be printed at the start of execution.",
+    getDefaultValue: static () => false)
+{
+    Arity = ArgumentArity.ZeroOrOne,
+};
+rootCommand.Add(printNeededOption);
+
+var libraryOverridesOption = new Option<ImmutableDictionary<string, string>>(
+    name: "--set-library-path",
+    description: "Sets the path for a given library. Accepts values in the format <name>:<path>.",
+    parseArgument: result =>
+    {
+        var overrides = ImmutableDictionary.CreateBuilder<string, string>(
+            StringComparer.Ordinal,
+            StringComparer.Ordinal);
+
+        foreach (var token in result.Tokens)
+        {
+            if (!token.Value.Contains(':'))
+            {
+                result.ErrorMessage = $"library path override '{token.Value}' is not in the correct format. Expected <name>:<path>.";
+                return ImmutableDictionary<string, string>.Empty;
+            }
+
+            var colonPos = token.Value.IndexOf(':');
+            var name = token.Value[..colonPos];
+            var path = token.Value[(colonPos + 1)..];
+
+            if (!File.Exists(path))
+            {
+                result.ErrorMessage = $"path does not exist: {path}";
+                return ImmutableDictionary<string, string>.Empty;
+            }
+
+            if (!overrides.TryAdd(name, path))
+            {
+                result.ErrorMessage = $"multiple paths were provided for library: {name}";
+                return ImmutableDictionary<string, string>.Empty;
+            }
+        }
+
+        return overrides.ToImmutable();
+    })
+{
+    Arity = ArgumentArity.ZeroOrMore,
+};
+rootCommand.Add(libraryOverridesOption);
+
+var outputOption = new Option<FileInfo?>(
+    name: "--output",
+    description: "Sets the path for a given library. Accepts values in the format <name>:<path>.",
+    getDefaultValue: () => null)
+{
+    Arity = ArgumentArity.ZeroOrOne,
+};
+outputOption.AddAlias("-o");
+rootCommand.Add(outputOption);
+
 rootCommand.SetHandler((InvocationContext context) =>
 {
     var console = context.Console;
-    var shouldPrintImports = context.ParseResult.GetValueForOption(printImportsOption);
+    var shouldPrintNeeded = context.ParseResult.GetValueForOption(printNeededOption);
+    var libraryOverrides = context.ParseResult.GetValueForOption(libraryOverridesOption)
+                           ?? ImmutableDictionary<string, string>.Empty;
     var assembly = context.ParseResult.GetValueForArgument(assemblyPathArgument);
-
+    var output = context.ParseResult.GetValueForOption(outputOption) ?? assembly;
+    var modified = false;
     var fileService = new ByteArrayFileService();
+
+    if (output.Directory is null || !output.Directory.Exists)
+    {
+        console.Error.WriteLine("error: output directory does not exist.");
+        context.ExitCode = ExitCodes.OutputDirectoryDoesNotExist;
+        return;
+    }
 
     PEFile peFile;
     PEImage peImage;
@@ -59,17 +120,39 @@ rootCommand.SetHandler((InvocationContext context) =>
     catch (BadImageFormatException ex)
     {
         console.Error.WriteLine($"error: provided file is not a .NET file: {ex.Message.TrimEnd('.')}.");
-        context.ExitCode = -1;
+        context.ExitCode = ExitCodes.FileIsNotDotnetAssembly;
         return;
     }
 
-    if (shouldPrintImports)
+    var imports = AssemblyWalker.ListDllImports(assemblyDefinition)
+        .ToImmutableArray();
+
+    if (shouldPrintNeeded)
     {
-        var imports = AssemblyWalker.ListDllImports(assemblyDefinition)
-            .Select(library => library.Library)
-            .Distinct()
-            .Order();
-        console.WriteLine(string.Join(Environment.NewLine, imports));
+        console.WriteLine(string.Join(
+            Environment.NewLine,
+            imports.Select(library => library.Library)
+                .Distinct()
+                .Order()));
+    }
+
+    if (libraryOverrides.Count > 0)
+    {
+        for (int i = 0; i < imports.Length; i++)
+        {
+            PatchCil.Model.DllImportImport import = imports[i];
+            if (libraryOverrides.TryGetValue(import.Library, out var path))
+            {
+                import.Method.ImplementationMap!.Scope!.Name = path;
+                imports = imports.SetItem(i, import with { Library = path });
+                modified = true;
+            }
+        }
+    }
+
+    if (modified)
+    {
+        assemblyDefinition.Write(output.FullName);
     }
 
     context.ExitCode = 0;
