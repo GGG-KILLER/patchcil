2e994595fd148ef570e53ed3724f98c897c30876
GGG <gggkiller2@gmail.com>
2025-01-10 20:53:30 -0300

    Publish binary as AOT.

--- nix/package.nix
+++ nix/package.nix
@@ -4,19 +4,35 @@
   lib,
   buildDotnetModule,
   dotnetCorePackages,
+  stdenv,
 }:
 
 buildDotnetModule {
   pname = "patchcil";
   inherit version src;
 
+  nativeBuildInputs = [
+    stdenv.cc
+  ];
+
   projectFile = "src/PatchCil.csproj";
   nugetDeps = ./deps.nix;
 
   dotnet-sdk = dotnetCorePackages.sdk_9_0;
-  dotnet-runtime = dotnetCorePackages.runtime_9_0;
+  dotnet-runtime = null;
 
+  selfContainedBuild = true;
+  dotnetFlags = [
+    "-p:DebuggerSupport=false"
+    "-p:StripSymbols=true"
+    "-p:TrimmerRemoveSymbols=true"
+  ];
   executables = [ "patchcil" ];
+
+  postFixup = ''
+    # Remove debug symbols as they shouldn't have anything in them.
+    rm $out/lib/patchcil/patchcil.dbg
+  '';
 
   meta = {
     description = "A tool for modifying CIL assemblies.";
@@ -24,7 +40,14 @@ buildDotnetModule {
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ ggg ];
     mainProgram = "patchcil";
-    # We run pretty much wherever .NET will let us.
-    inherit (dotnetCorePackages.runtime_9_0.meta) platforms;
+    # There's no easy way to filter which platforms we have an ILCompiler for, so...
+    platforms = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+      "x86_64-windows"
+      "i686-windows"
+    ];
   };
 }
